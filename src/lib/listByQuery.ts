e557a93af2def372762b752cac4ccfbc75af7476
Dominik Biedebach <dominik@bdbch.com>
2025-05-11 22:10:14 +0200

    added jsdocs and docs

--- src/lib/listByQuery.ts
+++ src/lib/listByQuery.ts
@@ -2,6 +2,13 @@ import { exec } from 'child_process';
 import type { WingetListOutput } from '../types';
 import parseWingetListOutput from './parseWingetListOutput';
 
+/**
+ * Lists installed packages using the Winget CLI and parses the output.
+ *
+ * @param {string} [query] - An optional query to filter the list of packages.
+ * @returns {Promise<WingetListOutput[]>} A promise that resolves to an array of Winget list results.
+ * @throws {Error} If the Winget CLI command fails.
+ */
 export function listByQuery(query?: string): Promise<WingetListOutput[]> {
   const command = ['winget list'];
 
